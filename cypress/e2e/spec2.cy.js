783595c2577e0cc4c91ce4a63d00fa3baaaab57b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-28 11:13:38 -0400

    add spec2

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -10,14 +10,11 @@ describe('Todo app', () => {
     cy.get('.todos .item').should('have.length', 3)
   })
 
-  // this test is inefficient, it repeats the entire first test
-  // just to get 3 todos items just so it can delete one of them
   it('removes todos', () => {
+    const todos = ['write code', 'write tests', 'deploy']
+    localStorage.setItem('todos-state-based-1', JSON.stringify({ todos }))
+
     cy.visit('/')
-    cy.get('input#todo')
-      .type('write code{enter}')
-      .type('write tests{enter}')
-      .type('deploy{enter}')
     cy.get('.todos .item').should('have.length', 3)
     cy.contains('.item', 'write tests').contains('button', 'Delete').click()
     cy.get('.todos .item').should('have.length', 2)
